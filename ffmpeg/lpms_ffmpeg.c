4aeae094a2bd75c1ed1f72cf872cc3d0c2e8436b
Josh Allmann <joshua.allmann@gmail.com>
2018-02-09 00:00:56 -0800

    ffmpeg: Transcoder skeleton.
    Sets up the Cgo interface and the basic transcoder structure.

--- ffmpeg/lpms_ffmpeg.c
+++ ffmpeg/lpms_ffmpeg.c
@@ -2,10 +2,20 @@
 
 #include <libavformat/avformat.h>
 
+//
+// Internal transcoder data structures
+//
+struct input_ctx {
+};
+
+struct output_ctx {
+  char *fname;         // required output file name
+  int width, height, bitrate; // w, h, br required
+  AVRational fps;
+};
+
 void lpms_init()
 {
-  av_register_all();
-  avcodec_register_all();
   avformat_network_init();
   av_log_set_level(AV_LOG_WARNING);
 }
@@ -14,6 +24,10 @@ void lpms_deinit()
 {
   avformat_network_deinit();
 }
+
+//
+// Segmenter
+//
 
 int lpms_rtmp2hls(char *listen, char *outf, char *ts_tmpl, char* seg_time)
 {
@@ -101,4 +115,154 @@ handle_r2h_err:
   if (oc) avformat_free_context(oc);
   if (md) av_dict_free(&md);
   return ret == AVERROR_EOF ? 0 : ret;
+}
+
+//
+// Transcoder
+//
+
+static void free_output(struct output_ctx *octx)
+{
+}
+
+
+static int open_output(struct output_ctx *octx, struct input_ctx *ictx)
+{
+#define em_err(msg) { \
+  if (!ret) ret = -1; \
+  fprintf(stderr, msg); \
+  goto open_output_err; \
+}
+  int ret = 0;
+
+  return 0;
+
+open_output_err:
+  free_output(octx);
+  return ret;
+}
+
+static void free_input(struct input_ctx *inctx)
+{
+}
+
+static int open_input(char *inp, struct input_ctx *ctx)
+{
+#define dd_err(msg) { \
+  if (!ret) ret = -1; \
+  fprintf(stderr, msg); \
+  goto open_input_err; \
+}
+  int ret = 0;
+
+  return 0;
+
+open_input_err:
+  free_input(ctx);
+  return ret;
+#undef dd_err
+}
+
+int process_in(struct input_ctx *ictx, AVPacket *pkt)
+{
+#define dec_err(msg) { \
+  if (!ret) ret = -1; \
+  fprintf(stderr, msg); \
+  goto dec_cleanup; \
+}
+  int ret = 0;
+
+  av_init_packet(pkt);
+
+dec_cleanup:
+  if (ret < 0) av_packet_unref(pkt); // XXX necessary? or have caller do it?
+  return ret;
+
+#undef dec_err
+}
+
+int process_out(struct output_ctx *octx, AVPacket *pkt)
+{
+#define proc_err(msg) { \
+  char errstr[AV_ERROR_MAX_STRING_SIZE] = {0}; \
+  if (!ret) { fprintf(stderr, "u done messed up\n"); ret = AVERROR(ENOMEM); } \
+  if (ret < -1) av_strerror(ret, errstr, sizeof errstr); \
+  fprintf(stderr, "%s: %s", msg, errstr); \
+  goto proc_cleanup; \
+}
+  int ret = 0;
+
+proc_cleanup:
+  return ret;
+#undef proc_err
+}
+
+#define MAX_OUTPUT_SIZE 10
+
+int lpms_transcode(char *inp, output_params *params, int nb_outputs)
+{
+#define main_err(msg) { \
+  if (!ret) ret = AVERROR(EINVAL); \
+  fprintf(stderr, msg); \
+  goto transcode_cleanup; \
+}
+  int ret = 0, i = 0;
+  struct input_ctx ictx;
+  AVPacket ipkt;
+  struct output_ctx outputs[MAX_OUTPUT_SIZE];
+  AVFrame *dframe;
+
+  memset(&ictx, 0, sizeof ictx);
+  memset(outputs, 0, sizeof outputs);
+
+  if (nb_outputs > MAX_OUTPUT_SIZE) main_err("transcoder: Too many outputs\n");
+
+  // populate input context
+  ret = open_input(inp, &ictx);
+  if (ret < 0) main_err("transcoder: Unable to open input\n");
+
+  // populate output contexts
+  for (i = 0; i < nb_outputs; i++) {
+    struct output_ctx *octx = &outputs[i];
+    octx->fname = params[i].fname;
+    octx->width = params[i].w;
+    octx->height = params[i].h;
+    if (params[i].bitrate) octx->bitrate = params[i].bitrate;
+    if (params[i].fps.den) octx->fps = params[i].fps;
+    ret = open_output(octx, &ictx);
+    if (ret < 0) main_err("transcoder: Unable to open output\n");
+  }
+
+  av_init_packet(&ipkt);
+
+  while (0) {
+    AVStream *ist = NULL;
+    ret = process_in(&ictx, &ipkt);
+    if (ret == AVERROR_EOF) break;
+    else if (ret < 0) goto whileloop_end; // XXX fix
+
+    for (i = 0; i < nb_outputs; i++) {
+      struct output_ctx *octx = &outputs[i];
+
+      ret = process_out(octx, &ipkt);
+      if (AVERROR(EAGAIN) == ret || AVERROR_EOF == ret) continue;
+      else if (ret < 0) main_err("transcoder: verybad\n");
+    }
+
+whileloop_end:
+    av_packet_unref(&ipkt);
+  }
+
+  // flush outputs
+  for (i = 0; i < nb_outputs; i++) {
+    struct output_ctx *octx = &outputs[i];
+    // only issue w this flushing method is it's not necessarily sequential
+    // wrt all the outputs; might want to iterate on each output per frame?
+  }
+
+transcode_cleanup:
+  free_input(&ictx);
+  for (i = 0; i < MAX_OUTPUT_SIZE; i++) free_output(&outputs[i]);
+  return ret == AVERROR_EOF ? 0 : ret;
+#undef main_err
 }
