cae4bc2b420f42fe27e4b468613dbe7cb0cefb77
Josh Allmann <joshua.allmann@gmail.com>
2018-02-16 18:57:01 -0800

    transcoder: Adjust test expectations to match the new reality.
    Encoding audio in AAC uses >40% fewer audio bits over MP2.

--- transcoder/ffmpeg_segment_transcoder_test.go
+++ transcoder/ffmpeg_segment_transcoder_test.go
@@ -34,15 +34,15 @@ func TestTrans(t *testing.T) {
 		t.Errorf("Expecting 2 output segments, got %v", len(r))
 	}
 
-	if len(r[0]) < 400000 || len(r[0]) > 550000 {
-		t.Errorf("Expecting output size to be between 400000 and 550000, got %v", len(r[0]))
+	if len(r[0]) < 250000 || len(r[0]) > 280000 {
+		t.Errorf("Expecting output size to be between 250000 and 280000 , got %v", len(r[0]))
 	}
 
-	if len(r[1]) < 500000 || len(r[1]) > 600000 {
-		t.Errorf("Expecting output size to be between 50000 and 600000, got %v", len(r[1]))
+	if len(r[1]) < 280000 || len(r[1]) > 310000 {
+		t.Errorf("Expecting output size to be between 280000 and 310000 , got %v", len(r[1]))
 	}
 
-	if len(r[2]) < 800000 || len(r[2]) > 950000 {
-		t.Errorf("Expecting output size to be between 800000 and 950000, got %v", len(r[2]))
+	if len(r[2]) < 600000 || len(r[2]) > 700000 {
+		t.Errorf("Expecting output size to be between 600000 and 700000, got %v", len(r[2]))
 	}
 }
