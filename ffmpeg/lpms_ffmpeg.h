8f39850a0cc1114eeb77d3e71fa2eed98f61ca87
Josh Allmann <joshua.allmann@gmail.com>
2019-04-24 06:10:43 +0000

    ffmpeg: Remove unused declarations from headers.
    Also add some include guards.

--- ffmpeg/lpms_ffmpeg.h
+++ ffmpeg/lpms_ffmpeg.h
@@ -1,3 +1,6 @@
+#ifndef _LPMS_FFMPEG_H_
+#define _LPMS_FFMPEG_H_
+
 #include <libavutil/rational.h>
 
 typedef struct {
@@ -7,7 +10,7 @@ typedef struct {
 } output_params;
 
 void lpms_init();
-void lpms_deinit();
 int  lpms_rtmp2hls(char *listen, char *outf, char *ts_tmpl, char *seg_time, char *seg_start);
 int  lpms_transcode(char *inp, output_params *params, int nb_outputs);
-int  lpms_length(char *inp, int ts_max, int packet_max);
+
+#endif // _LPMS_FFMPEG_H_
