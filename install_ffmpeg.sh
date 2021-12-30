a89fad9d802e922d3226dfa5e35e6e6360520e53
gpu <oscar_davids@outlook.com>
2021-12-29 23:59:42 -0500

    changed FFmpeg checkout version

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -30,7 +30,7 @@ if [ ! -e "$HOME/x264/x264" ]; then
   git clone http://git.videolan.org/git/x264.git "$HOME/x264"
   cd "$HOME/x264"
   # git master as of this writing
-  git checkout e0eebeeeddf863f72da0232f9dddc05200340560
+  git checkout 545de2ffec6ae9a80738de1b2c8cf820249a2530
   ./configure --prefix="$HOME/compiled" --enable-pic --enable-static
   make
   make install-lib-static
@@ -65,7 +65,7 @@ if [ ! -e "$HOME/ffmpeg/libavcodec/libavcodec.a" ]; then
   git clone https://github.com/livepeer/FFmpeg.git "$HOME/ffmpeg" || echo "FFmpeg dir already exists"
   cd "$HOME/ffmpeg"
 
-  git checkout 682c4189d8364867bcc49f9749e04b27dc37cded 
+  git checkout e0eebeeeddf863f72da0232f9dddc05200340560 
 
   ./configure --prefix="$HOME/compiled" --enable-libx264 --enable-gpl --enable-static $EXTRA_FFMPEG_FLAGS
   make
