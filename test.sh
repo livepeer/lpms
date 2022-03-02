94d891a08e1e2324a3b494d26c5acbd874401b22
JamesWanglf <jameswanglf@outlook.com>
2022-03-02 18:27:46 -0500

    Add github actions for lpms

--- test.sh
+++ test.sh
@@ -2,8 +2,16 @@
 
 set -eux
 
-go test ./...
+EXTRA_BUILD_TAGS=""
+DEVICE_FLAGS="sw"
 
-go run cmd/transcoding/transcoding.go transcoder/test.ts P144p30fps16x9,P240p30fps16x9 sw
+if which clang > /dev/null; then
+    EXTRA_BUILD_TAGS="--tags=nvidia"
+    DEVICE_FLAGS="nv 0"
+fi
+
+go test $EXTRA_BUILD_TAGS -timeout 30m ./...
+
+go run cmd/transcoding/transcoding.go transcoder/test.ts P144p30fps16x9,P240p30fps16x9 $DEVICE_FLAGS
 
 printf "\n\nAll Tests Passed\n\n"
